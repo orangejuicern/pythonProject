3d14ab7871e65d7105c8848fa7bc5f5493066508
orangejuicerightnow <owenhilljoyce@gmail.com>
2021-09-28 15:40:13 -0400

    test? i don't know

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint
+    print('hey there.')
+    print("You have been chosen for FREE IPHONE X GIVEAWAY AT jhsfdjksdfhkjsdhfsj dot com!!!")
+    print("omg really?!?!?!?!?!?!!?!?!?!?!?!?!!!11/1/!/!??!?!")
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('new user.')
+
+
